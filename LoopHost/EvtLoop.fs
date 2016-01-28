ecd3895e7b7a8588ef6289534ed018cb29f4b7de
rozgo <alex.rozgo@gmail.com>
2016-01-27 23:33:28 -0600

    more interop tests

--- LoopHost/EvtLoop.fs
+++ LoopHost/EvtLoop.fs
@@ -7,3 +7,25 @@ extern void hello_world ()
 
 [<DllImport("evtloop.dll", CallingConvention = CallingConvention.Cdecl)>]
 extern int32 get_num ()
+
+[<DllImport("evtloop.dll", CallingConvention = CallingConvention.Cdecl)>]
+extern int32* get_array ()
+
+type Combine = delegate of int32 * int32 -> int32
+
+[<DllImport("evtloop.dll", CallingConvention = CallingConvention.Cdecl)>]
+extern int32 combine (Combine)
+
+
+let test () =
+
+    let combine_mul = new Combine (fun a b -> a * b)
+    let combine_add = new Combine (fun a b -> a + b)
+
+    printfn "Hello world!"
+    printfn "%A" (System.IO.Directory.GetCurrentDirectory())
+    hello_world ()
+    printfn "Num from rust: %i" (get_num ())
+    printfn "Array from rust: %A" (get_array ())
+    printfn "Combine 2 3 *: %A" (combine combine_mul)
+    printfn "Combine 2 3 +: %A" (combine combine_add)
--- evtloop/src/lib.rs
+++ evtloop/src/lib.rs
@@ -9,6 +9,21 @@ pub extern "C" fn get_num() -> i32 {
     52
 }
 
+#[no_mangle]
+pub extern "C" fn get_array() -> [u8; 5] {
+    let xs: [u8; 5] = [1, 2, 3, 4, 5];
+    xs
+}
+
+// fn mult(a:i32, b:i32) -> i32 {
+//     a * b
+// }
+
+#[no_mangle]
+pub extern "C" fn combine(func : extern "C" fn(i32,i32) -> i32) -> i32 {
+    func(2,3)
+}
+
 #[cfg(test)]
 mod test {
     #[test]
